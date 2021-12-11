67d77d8b5d48bb40a320a084ae173da73818eabd
Sunday <82038537+SundayLee109@users.noreply.github.com>
2021-12-11 18:55:22 +0800

    Update a011.cpp

--- a/a011.cpp
+++ a/a011.cpp
@@ -2,14 +2,14 @@
 #include <string>
 using namespace std;
 int main(){
-    string s;
-    while (getline(cin, s)){
-        int n=0, i;
-        int len=s.length();
-        for (i=0; i<len; i++){
-            if (isalpha(s[i])!=0 && isalpha(s[i-1])==0){
-                n++;
-            }
+	string s;
+	while (getline(cin, s)){
+		int n=0, i;
+		int len=s.length();
+		for (i=0; i<len; i++){
+			if (isalpha(s[i])!=0 && isalpha(s[i-1])==0){
+				n++;
+			}
 		}
 		cout << n <<endl;
 	}
