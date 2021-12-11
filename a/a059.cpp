6a070f9a1079f5f7c05f639c737458debb18fc47
Sunday <82038537+SundayLee109@users.noreply.github.com>
2021-12-11 19:26:19 +0800

    Update a059.cpp

--- a/a059.cpp
+++ a/a059.cpp
@@ -3,23 +3,23 @@
 #include <cmath>
 using namespace std;
 int main(){
-int n,count=0;
-double wtf;
-cin>>n;
-int test[n][2];
-for(int i=0;i<n;i++){
-for(int j=0;j<2;j++){
-cin>>test[i][j];
-}
-}
-for(int i=0;i<n;i++){
-for(int j=test[i][0];j<=test[i][1];j++){
-if(sqrt((double)j)-(int)sqrt(j)==0){
-count=count+j;
-}
-}
-cout<<"Case "<<i+1<<": "<<count<<endl;
-count=0;
-}
-return 0;
+	int n,count=0;
+	double wtf;
+	cin>>n;
+	int test[n][2];
+	for(int i=0;i<n;i++){
+		for(int j=0;j<2;j++){
+			cin>>test[i][j];
+		}
+	}
+	for(int i=0;i<n;i++){
+		for(int j=test[i][0];j<=test[i][1];j++){
+			if(sqrt((double)j)-(int)sqrt(j)==0){
+				count=count+j;
+			}
+		}
+		cout<<"Case "<<i+1<<": "<<count<<endl;
+		count=0;
+	}
+	return 0;
 }
