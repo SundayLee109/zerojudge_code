25ff5e160490741b509f0902a390c525067abe1f
Sunday <82038537+SundayLee109@users.noreply.github.com>
2021-12-11 18:51:32 +0800

    Update a011.cpp

--- a/a011.cpp
+++ a/a011.cpp
@@ -1,34 +1,17 @@
 #include <iostream>
-
 #include <string>
-
 using namespace std;
-
-
-
 int main(){
-
 string s;
-
 while (getline(cin, s)){
-
 int n=0, i;
-
 int len=s.length();
-
 for (i=0; i<len; i++){
-
 if (isalpha(s[i])!=0 && isalpha(s[i-1])==0){
-
 n++;
-
 }
-
 }
-
 cout << n <<endl;
-
 }
-
 }
-抄了一下但仔細看過，自己想法也是這樣
+//抄了一下但仔細看過，自己想法也是這樣
