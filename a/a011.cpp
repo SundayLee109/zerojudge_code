5ae29d08ad001d3b98da68b35b144aa55de5df51
Sunday <82038537+SundayLee109@users.noreply.github.com>
2021-12-11 18:51:08 +0800

    Create a011.cpp

---
+++ a/a011.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+#include <string>
+
+using namespace std;
+
+
+
+int main(){
+
+string s;
+
+while (getline(cin, s)){
+
+int n=0, i;
+
+int len=s.length();
+
+for (i=0; i<len; i++){
+
+if (isalpha(s[i])!=0 && isalpha(s[i-1])==0){
+
+n++;
+
+}
+
+}
+
+cout << n <<endl;
+
+}
+
+}
+抄了一下但仔細看過，自己想法也是這樣
